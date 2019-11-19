45eaa686b99a35a8a5c227505917258ec20eb986
Jon Limjap <jonlimjap@gmail.com>
2019-11-20 04:48:20 +0800

    Adding Pattern Matching - Tuple Patterns

--- src/PhinugCSharp8Demo/Program.cs
+++ src/PhinugCSharp8Demo/Program.cs
@@ -266,7 +266,26 @@ namespace PhinugCSharp8Demo
 
         private static void PatternMatchingTuplePatterns()
         {
-            
+            Console.WriteLine("Pattern Matching - Tuple Patterns");
+
+            Console.WriteLine($"rock, paper: {RockPaperScissorsTuplePatterns("rock", "paper")}");
+            Console.WriteLine($"rock, rock: {RockPaperScissorsTuplePatterns("rock", "rock")}");
+            Console.WriteLine($"rock, scissors: {RockPaperScissorsTuplePatterns("rock", "scissors")}");
+            Console.WriteLine($"scissors, paper: {RockPaperScissorsTuplePatterns("scissors", "paper")}");
+            Console.WriteLine($"bato, papel: {RockPaperScissorsTuplePatterns("bato", "papel")}");
+
+            static string RockPaperScissorsTuplePatterns(string first, string second)
+                => (first, second) switch
+                {
+                    ("rock", "paper") => "rock is covered by paper. Paper wins.",
+                    ("rock", "scissors") => "rock breaks scissors. Rock wins.",
+                    ("paper", "rock") => "paper covers rock. Paper wins.",
+                    ("paper", "scissors") => "paper is cut by scissors. Scissors wins.",
+                    ("scissors", "rock") => "scissors is broken by rock. Rock wins.",
+                    ("scissors", "paper") => "scissors cuts paper. Scissors wins.",
+                    (_, _) when first != second => "no match",
+                    (_, _) => "tie"
+                };
         }
 
         private static void End_Feature()
